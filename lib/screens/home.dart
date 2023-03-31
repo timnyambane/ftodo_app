994b8ff62969ad5e0c7abef6f08d29290598ccde
Timothy Nyambane <onyanchanyambane@gmail.com>
2023-03-31 09:49:25 -0700

    All CRUD operations working

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,14 +17,32 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List todos = [];
+  final tController = TextEditingController();
+  final dController = TextEditingController();
+  bool isChecked = false;
+
   @override
   void initState() {
     super.initState();
     fetchTodo();
   }
 
-  final tController = TextEditingController();
-  final dController = TextEditingController();
+  Future<void> fetchTodo() async {
+    const url = 'http://10.0.2.2:8000/api/todos/index';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      final result = json.toList();
+
+      setState(() {
+        todos = result;
+      });
+    } else {
+      errorSnackBar(context, "Failed to load data, try again later");
+    }
+  }
 
   void createNewTodo() {
     showDialog(
@@ -54,31 +72,14 @@ class _HomePageState extends State<HomePage> {
 
     if (response.statusCode == 200) {
       successSnackBar(context, "Added succesfully");
+      fetchTodo();
+
       tController.clear();
       dController.clear();
       Navigator.of(context).pop();
     } else {
       errorSnackBar(context, "Failed!");
       Navigator.of(context).pop();
-    }
-  }
-
-  Future<void> editTodo(int id, String title, String desc) async {
-    final body = {'title': title, 'desc': desc, 'completed': false};
-    final url = 'http://10.0.2.2:8000/api/todos/update/$id';
-    final uri = Uri.parse(url);
-
-    final response = await http.put(
-      uri,
-      body: jsonEncode(body),
-      headers: {'Content-Type': 'application/json'},
-    );
-
-    if (response.statusCode == 200) {
-      fetchTodo();
-      successSnackBar(context, "Successfully updated");
-    } else {
-      errorSnackBar(context, "Failed to edit");
     }
   }
 
@@ -107,6 +108,25 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Future<void> editTodo(int id, String title, String desc) async {
+    final body = {'title': title, 'desc': desc, 'completed': false};
+    final url = 'http://10.0.2.2:8000/api/todos/update/$id';
+    final uri = Uri.parse(url);
+
+    final response = await http.put(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      fetchTodo();
+      successSnackBar(context, "Successfully updated");
+    } else {
+      errorSnackBar(context, "Failed to edit");
+    }
+  }
+
   Future<void> deleteTodo(int id) async {
     final response = await http
         .delete(Uri.parse('http://10.0.2.2:8000/api/todos/delete/$id'));
@@ -118,79 +138,100 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Future<void> fetchTodo() async {
-    const url = 'http://10.0.2.2:8000/api/todos/index';
+  Future<void> checkUpdate(dynamic todo, int id) async {
+    final url = 'http://10.0.2.2:8000/api/todos/update/$id';
     final uri = Uri.parse(url);
-    final response = await http.get(uri);
-
-    if (response.statusCode == 200) {
-      final json = jsonDecode(response.body);
-      final result = json.toList();
-
-      setState(() {
-        todos = result;
-      });
-    } else {
-      errorSnackBar(context, "Failed to load data, try again later");
+    final response = await http.put(
+      uri,
+      body: jsonEncode(todo),
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to update todo.');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF3F3F3),
       appBar: AppBar(
         title: const Text("Todo App"),
         centerTitle: true,
       ),
-      body: RefreshIndicator(
-        onRefresh: fetchTodo,
-        child: ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (context, index) {
-            final todo = todos[index];
-            return ListTile(
-              leading: Checkbox(
-                  value: todo['completed'] == 1, onChanged: (value) {}),
-              title: Text(todo['title']),
-              subtitle: Text(todo['desc']),
-              trailing: PopupMenuButton(
-                onSelected: (value) {
-                  if (value == 'edit') {
-                    editExistingTodo(todo);
-                  } else if (value == 'delete') {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text("Confirm delete"),
-                            content: const Text(
-                                "Are you sure you want to delete this todo?"),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: const Text('Cancel'),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  deleteTodo(todo['id']);
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('Delete'),
-                              ),
-                            ],
-                          );
+      body: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              final todo = todos[index];
+              return Card(
+                elevation: 0,
+                child: ListTile(
+                  leading: Checkbox(
+                    value: todo['completed'] == 1,
+                    onChanged: (bool? newVal) async {
+                      if (newVal != null) {
+                        setState(() {
+                          isChecked = newVal;
                         });
-                  }
-                },
-                itemBuilder: (context) {
-                  return [
-                    const PopupMenuItem(value: 'edit', child: Text('Edit')),
-                    const PopupMenuItem(value: 'delete', child: Text('Delete')),
-                  ];
-                },
-              ),
-            );
-          },
+
+                        // Update the todo in the database
+                        todo['completed'] = isChecked ? 1 : 0;
+                        try {
+                          await checkUpdate(todo, todo['id']);
+                        } catch (e) {
+                          debugPrint('Error updating todo: $e');
+                          errorSnackBar(context, "Failed to update todo.");
+                        }
+                      }
+                    },
+                  ),
+                  title: Text(todo['title']),
+                  subtitle: Text(todo['desc']),
+                  trailing: PopupMenuButton(
+                    onSelected: (value) {
+                      if (value == 'edit') {
+                        editExistingTodo(todo);
+                      } else if (value == 'delete') {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text("Confirm delete"),
+                                content: const Text(
+                                    "Are you sure you want to delete this todo?"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text('Cancel'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      deleteTodo(todo['id']);
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Delete'),
+                                  ),
+                                ],
+                              );
+                            });
+                      }
+                    },
+                    itemBuilder: (context) {
+                      return [
+                        const PopupMenuItem(value: 'edit', child: Text('Edit')),
+                        const PopupMenuItem(
+                            value: 'delete', child: Text('Delete')),
+                      ];
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
